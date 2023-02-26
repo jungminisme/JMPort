d88c8b8cdfd4ebbbb2324eeeb7181955936d3c2b
jungminisme <jungminisme@gmail.com>
2023-02-26 21:31:02 +0900

    Test code insert

---
+++ test/DTTest.cpp
@@ -0,0 +1,12 @@
+#include "../common/JMDataType.h"
+
+using namespace JMLib;
+
+int main()
+{
+
+    printf( "Data size of int8 is %lu\n", sizeof( int8 ) );
+    printf( "Data size of uint8 is %lu\n", sizeof( uint8 ));
+
+    return 0;
+}
