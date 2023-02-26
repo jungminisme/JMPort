0371837897f2afa4375ed020a90a0ba35196c9de
jungminisme <jungminisme@gmail.com>
2023-02-26 21:23:36 +0900

    remote commit

---
+++ common/JMDataType.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+namespace JMLib
+{
+    typedef char int8;
+    typedef unsigned char uint8;
+    typedef short int16;
+    typedef unsigned short uint16;
+    typedef int int32;
+    typedef unsigned int uint32;
+
+    typedef float float32;
+    typedef double float64;
+
+    typedef std::wstring string;
+}
