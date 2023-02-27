faa8f6e2371f7ffec263ac5638f5008848f7657e
jungminisme <jungminisme@gmail.com>
2023-02-27 20:22:33 +0900

    test2

--- test/DTTest.cpp
+++ test/DTTest.cpp
@@ -1,4 +1,4 @@
-#include "JMDataType.h"
+#include "../common/JMDataType.h"
 
 using namespace JMLib;
 
@@ -8,5 +8,11 @@ int main()
     printf( "Data size of int8 is %lu\n", sizeof( int8 ) );
     printf( "Data size of uint8 is %lu\n", sizeof( uint8 ));
 
+    printf( "Data size of int16 is %lu\n", sizeof( int16 ) );
+    printf( "Data size of uint16 is %lu\n", sizeof( uint16 ));
+    printf( "Data size of int32 is %lu\n", sizeof( int32 ) );
+    printf( "Data size of uint32 is %lu\n", sizeof( uint32 ));
+    printf( "Data size of float32 is %lu\n", sizeof( float32 ) );
+    printf( "Data size of float64 is %lu\n", sizeof( float64 ));
     return 0;
 }
