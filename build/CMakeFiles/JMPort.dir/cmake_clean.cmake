c1e37cc99317155220712771a6930dd35c439185
jungminisme <jungminisme@gmail.com>
2023-03-04 18:40:07 +0900

    CMake 적용

---
+++ build/CMakeFiles/JMPort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JMPort.dir/main.cpp.o"
+  "CMakeFiles/JMPort.dir/main.cpp.o.d"
+  "JMPort"
+  "JMPort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JMPort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
