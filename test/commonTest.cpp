3c17e875c40d2a2b9bf4d29aa4885e60ce01fffc
jungminisme <jungminisme@gmail.com>
2023-03-07 22:19:45 +0900

    ..

---
+++ test/commonTest.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include "JMString.h"
+
+TEST(CommonTest, StringTest)
+{
+    JMLib::string aString(L"Hello");
+    EXPECT_STREQ( aString.c_str(), L"Hello" );
+    aString << L" World" ;
+    EXPECT_STREQ( aString.c_str(), L"Hello World" );
+    EXPECT_EQ( aString.Size(), 12 );
+}
