dd9faaa2d19525eae1d15251bd8e00d0427010af
jungminisme <jungminisme@gmail.com>
2023-03-07 19:54:43 +0900

    ..

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include "JMString.h"
 
 int main(int, char**) {
-    JMLib::string aString( L"Hello World!!");
+    JMLib::string aString( L"Hell .. world!!");
+    std::wcout << aString.c_str() << std::endl;
+
+    aString << L" and welcom to another world!!" ;
+
     std::wcout << aString.c_str() << std::endl;
 }
