0f3654018e4a976652f96b290ac393f65ad78a1e
jungminisme <jungminisme@gmail.com>
2023-03-07 22:37:42 +0900

    ..

--- test/commonTest.cpp
+++ test/commonTest.cpp
@@ -8,5 +8,6 @@ TEST(CommonTest, StringTest)
     EXPECT_STREQ( aString.c_str(), L"Hello" );
     aString << L" World" ;
     EXPECT_STREQ( aString.c_str(), L"Hello World" );
-    EXPECT_EQ( aString.Size(), 12 );
+    EXPECT_EQ( aString.Size(), 11 );
+
 }
