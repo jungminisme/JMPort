56b3376db78648fa40dae65184004f6fa50c857e
jungminisme <jungminisme@gmail.com>
2023-03-08 19:38:21 +0900

    String 추가 테스트 작성

--- test/commonTest.cpp
+++ test/commonTest.cpp
@@ -2,12 +2,31 @@
 
 #include "JMString.h"
 
-TEST(CommonTest, StringTest)
+TEST(StringTest, Constructor)
 {
+    JMLib::string aSam1;
+    EXPECT_STREQ(aSam1.c_str(), L"" );
     JMLib::string aString(L"Hello");
     EXPECT_STREQ( aString.c_str(), L"Hello" );
-    aString << L" World" ;
-    EXPECT_STREQ( aString.c_str(), L"Hello World" );
-    EXPECT_EQ( aString.Size(), 11 );
 
+    JMLib::string aSam2(aString);
+    EXPECT_STREQ( aSam2.c_str(), L"Hello" );
+}
+
+TEST(StringTest, Assign)
+{
+    JMLib::string aSam1;
+    aSam1 = L" World" ;
+    EXPECT_STREQ( aSam1.c_str(), L" World" );
+    JMLib::string aSam2;
+    aSam2.Assign( aSam1 );
+    EXPECT_STREQ( aSam2.c_str(), L" World" );
+
+    JMLib::string aSam3;
+    aSam3 = aSam1;
+    EXPECT_STREQ( aSam3.c_str(), L" World" );
+
+    JMLib::string aSam4;
+    aSam4 = L" World";
+    EXPECT_STREQ( aSam4.c_str(), L" World" );
 }
