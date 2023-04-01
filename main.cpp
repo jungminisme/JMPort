9841d22eaf080777b981483e4ff9cb9fab168181
jungminisme <jungminisme@gmail.com>
2023-04-01 12:10:57 +0900

    .. Not hell .. Hello world ..

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "JMString.h"
 
 int main(int, char**) {
-    JMLib::string aString( L"Hell .. world!!");
+    JMLib::string aString( L"Hello .. world!!");
     std::wcout << aString.c_str() << std::endl;
 
     aString << L" and welcom to another world!!" ;
